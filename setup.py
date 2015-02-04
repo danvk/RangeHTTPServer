ac9940345649971546a372d4e6170a0f71717c5e
Dan Vanderkam <danvdk@gmail.com>
2015-02-03 16:31:24 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(name='rangehttpserver',
+      version='1.0.0',
+      description='SimpleHTTPServer with support for Range requests',
+      author='Dan Vanderkam',
+      author_email='danvdk@gmail.com',
+      url='https://github.com/danvk/RangeHTTPServer/',
+      packages=find_packages(exclude=['tests*']),
+      install_requires=[],
+      classifiers=[
+          'Environment :: Console',
+          'Environment :: Web Environment',
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License'
+      ]
+)
---
+++ tests/RangeHTTPServer_test.py
@@ -0,0 +1,29 @@
+import RangeHTTPServer
+
+from nose.tools import *
+from StringIO import StringIO
+
+
+def test_parse_byte_range():
+    eq_((0, 499), RangeHTTPServer.parse_byte_range('bytes=0-499'))
+    eq_((987, 1024), RangeHTTPServer.parse_byte_range('bytes=987-1024'))
+
+
+def test_copy_byte_range():
+    inbuffer = StringIO('0123456789abcdefghijklmnopqrstuvwxyz')
+    outbuffer = StringIO()
+
+    RangeHTTPServer.copy_byte_range(inbuffer, outbuffer, 4, 10)
+    eq_('456789a', outbuffer.getvalue())
+
+    outbuffer = StringIO()
+    RangeHTTPServer.copy_byte_range(inbuffer, outbuffer, 0, 4)
+    eq_('01234', outbuffer.getvalue())
+
+    outbuffer = StringIO()
+    RangeHTTPServer.copy_byte_range(inbuffer, outbuffer, 26)
+    eq_('qrstuvwxyz', outbuffer.getvalue())
+
+    outbuffer = StringIO()
+    RangeHTTPServer.copy_byte_range(inbuffer, outbuffer, 0, 9, 10)
+    eq_('0123456789', outbuffer.getvalue())
