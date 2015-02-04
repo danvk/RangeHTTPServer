a30242dc08a412fea9d4a8caf1b482448f723fa5
Dan Vanderkam <danvdk@gmail.com>
2015-02-03 16:46:17 -0800

    long_description

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@ from setuptools import setup, find_packages
 setup(name='rangehttpserver',
       version='1.0.0',
       description='SimpleHTTPServer with support for Range requests',
+      long_description=(
+      '''Drop-in replacement for SimpleHTTPServer with support for HTTP Range requests.
+
+      Usage:
+
+          pip install rangehttpserver
+          python -m RangeHTTPServer
+'''),
       author='Dan Vanderkam',
       author_email='danvdk@gmail.com',
       url='https://github.com/danvk/RangeHTTPServer/',
