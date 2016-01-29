2c1d7408e4883b3d256b750afe4868d86aa60367
Dario Bertini <berdario@gmail.com>
2016-01-29 18:59:10 +0100

    Python2's SimpleHTTPServer.test doesn't support bind and port args, adapt the __main__ to it (also add Python3.5 to .travis.yml)

--- RangeHTTPServer.py
+++ RangeHTTPServer.py
@@ -115,13 +115,17 @@ class RangeRequestHandler(SimpleHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--bind', '-b', default='', metavar='ADDRESS',
-                        help='Specify alternate bind address '
-                             '[default: all interfaces]')
-    parser.add_argument('port', action='store',
-                        default=8000, type=int,
-                        nargs='?',
-                        help='Specify alternate port [default: 8000]')
-    args = parser.parse_args()
-    http_server.test(HandlerClass=RangeRequestHandler, port=args.port, bind=args.bind)
+    if sys.version_info[0] == 2:
+        http_server.test(HandlerClass=RangeRequestHandler)
+        # Python2's SimpleHTTPServer.test doesn't support bind and port args
+    else:
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--bind', '-b', default='', metavar='ADDRESS',
+                            help='Specify alternate bind address '
+                                 '[default: all interfaces]')
+        parser.add_argument('port', action='store',
+                            default=8000, type=int,
+                            nargs='?',
+                            help='Specify alternate port [default: 8000]')
+        args = parser.parse_args()
+        http_server.test(HandlerClass=RangeRequestHandler, port=args.port, bind=args.bind)
