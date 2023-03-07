e0510a3209c8ad598c0a9d3c8fa37d2a3ad8a1f7
Dan Vanderkam <danvdk@gmail.com>
2023-03-07 11:45:24 -0500

    set long_description for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+with open('README.md', encoding='utf8') as fh:
+    long_description = fh.read()
+
 setup(name='rangehttpserver',
-      version='1.3.0',
+      version='1.3.1',
       description='SimpleHTTPServer with support for Range requests',
+      long_description=long_description,
       author='Dan Vanderkam',
       author_email='danvdk@gmail.com',
       url='https://github.com/danvk/RangeHTTPServer/',
